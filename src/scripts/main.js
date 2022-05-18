d2d0f41aa591b270d3aa4c300f4384788b22a971
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-05-17 21:29:18 -0500

    added button and form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,66 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+// RENDER TO DOM
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender;
 };
 
-init();
+// API CALL
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+// UI presentation (HTML ON THE DOm)
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    document.querySelector('#app').innerHTML = response.lyrics;
+  });
+};
+
+// BUTTON
+const btnClick = () => {
+  let domString = '';
+  domString = `<button type="button" class="btn btn-primary"
+  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+Log lyrics
+</button>`;
+  renderToDom('#btnContainer', domString);
+};
+
+// FORM
+const inputForm = () => {
+  let domString = '';
+  domString = `<div class="input-group mb-3">
+  <span class="input-group-text" id="basic-addon1">@</span>
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+
+<label for="basic-url" class="form-label">Your opinion</label>
+
+<div class="input-group">
+  <span class="input-group-text">With textarea</span>
+  <textarea class="form-control" aria-label="With textarea"></textarea>
+</div>
+
+<button type="button" class="submitBtn"
+  style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+Submit
+</button>
+`;
+  renderToDom('#formContainer', domString);
+};
+
+const startApp = () => {
+  lyricsOnDom('nav', 'myself');
+  btnClick();
+  inputForm();
+};
+
+startApp();
